66b40bb3afad1f5fdd76221092b8b8f9aae23729
tushar0107 <tushardasare1701@gmail.com>
2023-12-11 18:06:00 +0530

    production ready changes

--- mystore/mystore/settings.py
+++ mystore/mystore/settings.py
@@ -132,3 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:8000',
+    'https://my-store-49td.onrender.com',
+]
